f04490573ee461e04cb48de1dbf8b6f5a62e48cc
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-06-29 23:40:46 +0100

    :sparkles: Finished the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
 import { withTranslation } from 'react-i18next'
 import Layout from '../components/layout'
 import '../styles/pages/404.scss'
@@ -7,9 +8,27 @@ import '../styles/pages/404.scss'
 const NotFound = ({ t }) => (
   <Layout>
     <div className="page-centered" role="article">
-      <h1>
-        {t('notFound')}
-      </h1>
+      <article className="error-container">
+        <h1
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={
+            {
+              __html: t(
+                'notFoundTitle',
+                {
+                  errorCode: '<span class="highlight" role="mark">404</span>',
+                },
+              ),
+            }
+          }
+        />
+        <p>
+          {t('notFoundContent')}
+        </p>
+        <Link to="/" className="button">
+          {t('notFoundButton')}
+        </Link>
+      </article>
     </div>
   </Layout>
 )
