28cb3aa7701e3cda22b92ceaa4449992c2fd6ec7
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-07-04 15:27:18 +0100

    :sparkles: Added the careers widget

--- src/components/home/careers.jsx
+++ src/components/home/careers.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
 import { withTranslation } from 'react-i18next'
 
-// eslint-disable-next-line no-unused-vars
-const Careers = ({ t }) => <h1>Careers</h1>
+const nToBR = (str) => {
+  if (typeof str === 'string') {
+    const content = []
+    str.split('\n').forEach((v, i, a) => {
+      content.push(v)
+      if (i + 1 < a.length) {
+        content.push(<br />)
+      }
+    })
+    return content
+  }
+  return str
+}
+
+const Careers = ({ t }) => (
+  <section id="careers">
+    <h3>{t('careersHeader')}</h3>
+    <p>{nToBR(t('careersText'))}</p>
+    <Link to="#top" className="cta">{t('careersCTA')}</Link>
+  </section>
+)
 
 Careers.propTypes = {
   t: PropTypes.func.isRequired,
