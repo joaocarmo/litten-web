2efcad3a97f6e093e305def854bff261cc59bbd6
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-07-18 18:37:46 +0100

    :heavy_plus_sign: External links open in a new window

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,9 @@ module.exports = {
               maintainCase: false,
             },
           },
+          {
+            resolve: 'gatsby-remark-external-links',
+          },
         ],
       },
     },
