ba7e5e748489a9cb47d4bc3db227b2a7eaeed6c1
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-12-19 18:46:23 +0000

    :sparkles: Added a redirect for link params

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,33 @@
+import config from './package.json'
 import './src/config/i18n'
 import 'normalize.css'
 import './src/styles/global.scss'
 
-// eslint-disable-next-line import/prefer-default-export
-export const onClientEntry = () => {
+const polyfillIntersectionObserver = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (!('IntersectionObserver' in window)) {
     import('intersection-observer')
   }
+}
+
+const redirectOnLink = ({ search } = {}) => {
+  if (search) {
+    const searchParams = new URLSearchParams(search)
+    const link = searchParams.get('link')
+    if (link) {
+      const homepageUrl = new URL(config.homepage)
+      const linkUrl = new URL(link)
+      if (linkUrl.hostname === homepageUrl.hostname) {
+        window.location.href = `${linkUrl.pathname}${linkUrl.search}`
+      }
+    }
+  }
+}
+
+export const onClientEntry = () => {
+  polyfillIntersectionObserver()
+}
+
+export const onRouteUpdate = ({ location }) => {
+  redirectOnLink(location)
 }
