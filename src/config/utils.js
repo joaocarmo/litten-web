00d2c11cf29cbf97478e66e3e1ca38c3fbbcd549
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2021-01-23 18:46:21 +0000

    :bug: Fixes the build

--- src/config/utils.js
+++ src/config/utils.js
@@ -1,5 +1,13 @@
 import Bowser from 'bowser'
 
+export const getUserAgent = () => {
+  if (typeof window !== 'undefined') {
+    return window.navigator.userAgent
+  }
+
+  return 'Zombie//1.0 (Server)'
+}
+
 export const buildIntent = (
   uriScheme,
   packageName,
@@ -13,11 +21,11 @@ export const buildIntent = (
   };end`
 
 export const useIntent = () => {
-  const browser = Bowser.getParser(window.navigator.userAgent)
+  const browser = Bowser.getParser(getUserAgent())
   return browser.satisfies({ mobile: { chrome: '>=25' } })
 }
 
 export const isIE11 = () => {
-  const browser = Bowser.getParser(window.navigator.userAgent)
+  const browser = Bowser.getParser(getUserAgent())
   return browser.satisfies({ windows: { 'internet explorer': '>10' } })
 }
