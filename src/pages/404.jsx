361945443aef19ca663769abf4385aa10378270a
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2022-05-30 12:20:29 +0100

    [Feat] New 404 page (#21)
    * :arrow_up: Upgrades a few dependencies
    
    * :sparkles: Adds a new 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,23 +3,29 @@ import { Link } from 'gatsby'
 import { Trans, withTranslation } from 'react-i18next'
 import Layout from '../components/layout'
 import { HOME } from '../config/link-refs'
+import badPuppy from '../images/404.svg'
 
 const NotFound = ({ t }) => (
   <Layout>
-    <div className="page-centered" role="article">
-      <article className="contained title-container">
+    <main className="not-found-grid">
+      <section className="not-found-text">
         <h1>
           <Trans i18nKey="notFoundTitle" values={{ errorCode: '404' }}>
             {/* eslint-disable */}
-            Sorry about the <mark>errorCode</mark>!{/* eslint-enable */}
+            Lost Litten <mark>errorCode</mark>!{/* eslint-enable */}
           </Trans>
         </h1>
         <p>{t('notFoundContent')}</p>
+      </section>
+      <article className="not-found-image">
+        <img src={badPuppy} alt="404" className="not-found" />
+      </article>
+      <nav className="not-found-button">
         <Link to={HOME} className="button">
           {t('notFoundButton')}
         </Link>
-      </article>
-    </div>
+      </nav>
+    </main>
   </Layout>
 )
 
