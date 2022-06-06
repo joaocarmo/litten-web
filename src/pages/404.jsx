629260d9877894529dffaafeeb5179a9282e10d3
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2022-06-06 20:07:31 +0200

    [Feat] Add random 404 (#22)
    * :arrow_up: Upgrade a few dependencies
    
    * :sparkles: Add random messages to the 404 page
    
    * :bug: Fix the flickering bug
    
    * :bug: Changed the logic to get the random quote

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import { Trans, withTranslation } from 'react-i18next'
@@ -5,29 +6,40 @@ import Layout from '../components/layout'
 import { HOME } from '../config/link-refs'
 import badPuppy from '../images/404.svg'
 
-const NotFound = ({ t }) => (
-  <Layout>
-    <main className="not-found-grid">
-      <section className="not-found-text">
-        <h1>
-          <Trans i18nKey="notFoundTitle" values={{ errorCode: '404' }}>
-            {/* eslint-disable */}
-            Lost Litten <mark>errorCode</mark>!{/* eslint-enable */}
-          </Trans>
-        </h1>
-        <p>{t('notFoundContent')}</p>
-      </section>
-      <article className="not-found-image">
-        <img src={badPuppy} alt="404" className="not-found" />
-      </article>
-      <nav className="not-found-button">
-        <Link to={HOME} className="button">
-          {t('notFoundButton')}
-        </Link>
-      </nav>
-    </main>
-  </Layout>
-)
+const NotFound = ({ t }) => {
+  const notFoundContent = useMemo(() => {
+    const notFoundRandom = t('notFoundContent', {
+      returnObjects: true,
+    })
+    const randomIndex = Math.floor(Math.random() * notFoundRandom.length)
+
+    return notFoundRandom[randomIndex]
+  }, [t])
+
+  return (
+    <Layout>
+      <main className="not-found-grid">
+        <section className="not-found-text">
+          <h1>
+            <Trans i18nKey="notFoundTitle" values={{ errorCode: '404' }}>
+              {/* eslint-disable */}
+              Lost Litten <mark>errorCode</mark>!{/* eslint-enable */}
+            </Trans>
+          </h1>
+          <p>{notFoundContent}</p>
+        </section>
+        <article className="not-found-image">
+          <img src={badPuppy} alt="404" className="not-found" />
+        </article>
+        <nav className="not-found-button">
+          <Link to={HOME} className="button">
+            {t('notFoundButton')}
+          </Link>
+        </nav>
+      </main>
+    </Layout>
+  )
+}
 
 NotFound.propTypes = {
   t: PropTypes.func.isRequired,
