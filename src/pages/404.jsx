661f80548bd23bc284004d22c2f0c04dc8a94166
João Carmo <kingjoao@gmail.com>
2022-06-06 18:31:33 +0100

    :sparkles: Add random messages to the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,29 +5,36 @@ import Layout from '../components/layout'
 import { HOME } from '../config/link-refs'
 import badPuppy from '../images/404.svg'
 
-const NotFound = ({ t }) => (
-  <Layout>
-    <main className="not-found-grid">
-      <section className="not-found-text">
-        <h1>
-          <Trans i18nKey="notFoundTitle" values={{ errorCode: '404' }}>
-            {/* eslint-disable */}
-            Lost Litten <mark>errorCode</mark>!{/* eslint-enable */}
-          </Trans>
-        </h1>
-        <p>{t('notFoundContent')}</p>
-      </section>
-      <article className="not-found-image">
-        <img src={badPuppy} alt="404" className="not-found" />
-      </article>
-      <nav className="not-found-button">
-        <Link to={HOME} className="button">
-          {t('notFoundButton')}
-        </Link>
-      </nav>
-    </main>
-  </Layout>
-)
+const NotFound = ({ t }) => {
+  const notFoundRandom = t('notFoundContent', {
+    returnObjects: true,
+  })
+  const randomIndex = Math.floor(Math.random() * notFoundRandom.length)
+
+  return (
+    <Layout>
+      <main className="not-found-grid">
+        <section className="not-found-text">
+          <h1>
+            <Trans i18nKey="notFoundTitle" values={{ errorCode: '404' }}>
+              {/* eslint-disable */}
+              Lost Litten <mark>errorCode</mark>!{/* eslint-enable */}
+            </Trans>
+          </h1>
+          <p>{notFoundRandom[randomIndex]}</p>
+        </section>
+        <article className="not-found-image">
+          <img src={badPuppy} alt="404" className="not-found" />
+        </article>
+        <nav className="not-found-button">
+          <Link to={HOME} className="button">
+            {t('notFoundButton')}
+          </Link>
+        </nav>
+      </main>
+    </Layout>
+  )
+}
 
 NotFound.propTypes = {
   t: PropTypes.func.isRequired,
