b8424cf72cd7636fb9f5578d1d8d7cfb732529a7
João Carmo <kingjoao@gmail.com>
2022-07-02 15:03:12 +0100

    :arrow_up: Upgrades a few dependencies

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
   customSyntax: 'postcss-scss',
   rules: {
