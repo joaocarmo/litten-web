a599d3f8c1f237b6f9bdc11f1e90432b9604a421
João Carmo <kingjoao@gmail.com>
2023-12-31 17:20:14 +0000

    feat(language-selector): add translation keys and Russian language option

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,12 +1,19 @@
 export enum Language {
   EN = 'en',
-  PT = 'pt',
   ES = 'es',
+  PT = 'pt',
+  RU = 'ru',
 }
 
 const locales = {
   langs: Object.values(Language),
   defaultLangKey: Language.EN,
+  translationKeys: {
+    [Language.EN]: 'langEnglish',
+    [Language.ES]: 'langSpanish',
+    [Language.PT]: 'langPortuguese',
+    [Language.RU]: 'langRussian',
+  },
 }
 
 export default locales
